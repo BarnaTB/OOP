8af10efdd659ecfa4ffe345402d0798927905e21
BarnaTB <barnabastb2@gmail.com>
2018-09-24 10:31:24 +0300

    ft(User should be able to check if a dog is hungry): Commit delivers a feature that enables a user to check if a dog is hungry. [Deliver #160715342]

--- pets_class.py
+++ pets_class.py
@@ -9,3 +9,12 @@ class Dog:
     def eat(self):
         self.is_hungry = False
         return self.is_hungry
+
+    def check_for_hunger(self):
+        for dog in Pets.dogs:
+            if dog.is_hungry:
+                return 'My dogs are hungry'
+            return 'My dogs are not hungry'
+
+print('I have {0} dogs'.format(len(Pets.dogs)))
+print('And they\'re all mammals, of course.')
